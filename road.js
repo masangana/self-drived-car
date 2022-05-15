c197adce97a48b0f17cf56931bcf06aada1c7e47
masangana <alexmasangana@gmail.com>
2022-05-15 15:17:46 +0300

    add road lane
    adding road lane in road.js

--- road.js
+++ road.js
@@ -13,6 +13,12 @@ class Road{
 
     }
 
+    getLaneCenter(laneIndex){
+        const laneWidth = this.width/this.laneCount;
+        return this.left+laneWidth/2 + 
+        Math.min(laneIndex,this.laneCount-1 )* laneWidth;
+    }
+
     draw(ctx){
         ctx.lineWidth = 5;
         ctx.strokeStyle = "white";
