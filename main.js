38a826c959e3047a23589946d6513ef8f85406e1
masangana <alexmasangana@gmail.com>
2022-05-15 15:52:35 +0300

    add borders to the road && fix the car on the road
    --fix the car on the bottom of the road
    --add road borders
    -- change the road lane numbers

--- main.js
+++ main.js
@@ -4,15 +4,21 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2, canvas.width*0.8);
-const car = new Car(road.getLaneCenter(2),100,20,50);
+const car = new Car(road.getLaneCenter(1),100,30,45);
 car.draw(ctx);
 
 animate();
 
 function animate() {
     car.update();
+
     canvas.height = window.innerHeight;
-    road.draw(ctx)
+
+    ctx.save();
+    ctx.translate(0, -car.y + canvas.height*0.6);
+    road.draw(ctx);
     car.draw(ctx);
+
+    ctx.restore();
     requestAnimationFrame(animate);
 }
